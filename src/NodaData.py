804af942e774cdfcaa1ecb261a0115499c1b0261
Nadav-Eps <nadavepstein1@gmail.com>
2020-12-28 11:43:03 +0200

    ondedata

---
+++ src/NodaData.py
@@ -0,0 +1,34 @@
+from functools import cmp_to_key
+import numpy as np
+
+Counter = 0
+
+
+class NodeData:
+
+    def __init__(self, X=0, Y=0, Z=0, Tag=-1, Weight=-1, Key=Counter, Info=""):
+        global Counter
+        Counter = Counter + 1
+        self.Info = Info
+        self.Pos = (X, Y, Z)
+        self.Tag = Tag
+        self.Weight = Weight
+        self.Key = Key
+
+    def comp(self, other) -> int:
+        if self.Weight < other.Weight:
+            return -1
+        elif self.Weight > other.Weight:
+            return 1
+        else:
+            return 0
+
+    def distance(self, other) -> float:
+        return np.sqrt(
+            (self.pos[0] - other.pos[0]) ** 2 + (self.pos[1] - other.pos[1]) ** 2 + (self.pos[2] - other.pos[2]) ** 2)
+
+    def __eq__(self, other):
+        return self.Key == other.Key and \
+               self.Tag == other.Tag and \
+               self.Weight == other.Weight and \
+               self.Pos == other.Pos
