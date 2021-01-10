e1f4006f673d018da80dabd6b02155c52ac72486
or trabelsi <ortrsa@gmail.com>
2021-01-10 23:57:06 +0200

    img

--- src/correctness.py
+++ src/correctness.py
@@ -12,53 +12,68 @@ from network import Network
 import time
 
 if __name__ == '__main__':
-    file_list = ["G_30000_240000_0.json", "G_20000_160000_0.json", "G_10000_80000_0.json", "G_1000_8000_0.json",
-             "G_100_800_0.json", "G_10_80_0.json"]
+    file_list = ["../tests/G_30000_240000_1.json", "../tests/G_20000_160000_1.json", "../tests/G_10000_80000_1.json",
+                 "../tests/G_1000_8000_1.json",
+                 "../tests/G_100_800_1.json", "../tests/G_10_80_1.json"]
+
+    a1 = []
+    a2 = []
+    a3 = []
+    a4 = []
+    a5 = []
+    a6 = []
 
     for i in file_list:
         my_g = GraphAlgo()
         my_g.load_from_json(i)
         netx = Network(i)
+        my_g.save_to_json(i + "Ex2")
 
-        print("----",i,"----")
-        print("--","connected_component(3)","--" )
+        print("----", i, "----")
+
+        print("--", "connected_component(3)", "--")
         my_time_s = time.time()
         cc3 = my_g.connected_component(3)
         my_time_e = time.time()
-        print("Ex3, len of connected component to 3",len(cc3), " time: ", my_time_e - my_time_s)
+        print("Ex3, len of connected component to 3", len(cc3), " time: ", my_time_e - my_time_s)
+        a1.append(my_time_e - my_time_s)
 
         my_time_s = time.time()
         cc3nx = [i for i in nx.strongly_connected_components(netx.G) if 3 in i]
         my_time_e = time.time()
-        print("nx,  len of connected component to 3",len(cc3nx[0])," time: ", my_time_e - my_time_s)
-        print("--", "connected_components()","--")
+        print("nx,  len of connected component to 3", len(cc3nx[0]), " time: ", my_time_e - my_time_s)
+        a2.append(my_time_e - my_time_s)
+
+        print("--", "connected_components()", "--")
         my_time_s = time.time()
         ccs = my_g.connected_components()
         my_time_e = time.time()
-        print("Ex3, len of connected components",len(ccs)," time: " ,my_time_e - my_time_s)
+        print("Ex3, len of connected components", len(ccs), " time: ", my_time_e - my_time_s)
+        a3.append(my_time_e - my_time_s)
 
         my_time_s = time.time()
         ccsnx = [i for i in nx.strongly_connected_components(netx.G)]
         my_time_e = time.time()
-        print("nx,  len of connected components", len(ccsnx)," time: ", my_time_e - my_time_s)
-        print("--", "shortest path()","--")
+        print("nx,  len of connected components", len(ccsnx), " time: ", my_time_e - my_time_s)
+        a4.append(my_time_e - my_time_s)
+
+        print("--", "shortest path()", "--")
         my_time_s = time.time()
-        short = my_g.shortest_path(1,2)
+        short = my_g.shortest_path(1, 2)
         my_time_e = time.time()
-        print("Ex3, shortest path ",short," time: " ,my_time_e - my_time_s)
+        print("Ex3, shortest path ", short, " time: ", my_time_e - my_time_s)
+        a5.append(my_time_e - my_time_s)
 
         my_time_s = time.time()
-        shortnx = nx.dijkstra_path(netx.G,1,2)
+        shortnx = nx.dijkstra_path(netx.G, 1, 2)
         my_time_e = time.time()
-        print("nx,  shortest path ",short," time: " ,my_time_e - my_time_s)
-
+        print("nx,  shortest path ", short, " time: ", my_time_e - my_time_s)
+        a6.append(my_time_e - my_time_s)
         print()
 
-
-
-
-
-
-
-
-
+    print(a1)
+    print(a2)
+    print(a3)
+    print(a4)
+    print(a5)
+    print(a6)
--- src/main.py
+++ src/main.py
@@ -11,15 +11,12 @@ import json
 from network import Network
 
 if __name__ == '__main__':
-    filename = 'data/A5'
+    filename = 'tests/G_100_800_1.json'
     Galgo = GraphAlgo()
     netx = Network(filename)
     Galgo.load_from_json(filename)
-    Galgo.G.remove_node(5)
-    Galgo.G.remove_node(13)
-    Galgo.G.remove_node(14)
-    netx.G.remove_node(14)
-    print(netx.G.out_edges(14))
+
+    print(Galgo.shortest_path(0,4))
     # print(Galgo.G.all_in_edges_of_node(14))
     Galgo.plot_graph()
 
